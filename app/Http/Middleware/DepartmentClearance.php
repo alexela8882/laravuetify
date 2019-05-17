49ff6b06b1ce4a32786653b6995b4d8499ec329b
alexela8882 <alexela8882@gmail.com>
2019-05-17 15:11:58 +0800

    allow user employments to access department with the right permissions

--- app/Http/Middleware/DepartmentClearance.php
+++ app/Http/Middleware/DepartmentClearance.php
@@ -16,7 +16,11 @@ class DepartmentClearance
         } else {
           if (Auth::user()->hasAnyPermission('Create Departments',
                                              'Edit Departments',
-                                             'Delete Departments')) {
+                                             'Delete Departments',
+                                             'Show User Employments',
+                                             'Create User Employments',
+                                             'Edit User Employments',
+                                             'Delete User Employments')) {
             return $next($request);
           } else {
             return response()->json('Forbidden', 403);
